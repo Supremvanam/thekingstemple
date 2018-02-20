85714fa554cd0ffe6b45b0c9824ee64fd40c7208
Suprem Vanam <supremvanam.gsp@gmail.com>
2018-02-20 18:04:50 +0530

    Streak View

---
+++ TKT Church/Controller/WebKitVC.swift
@@ -0,0 +1,35 @@
+//
+//  WebKitVC.swift
+//  TKT Church
+//
+//  Created by Suprem Vanam on 19/01/18.
+//  Copyright © 2018 Suprem Vanam. All rights reserved.
+//
+
+import UIKit
+
+class WebKitVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
